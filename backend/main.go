1cc91b02e647e02f734c4c7f5262e4b2b5f1fa50
iisangil <iisangil@umich.edu>
2021-02-04 15:54:32 -0500

    removed comments from main.go

--- backend/main.go
+++ backend/main.go
@@ -15,10 +15,6 @@ type Message struct {
 	Channel  string `json:"channel"`
 }
 
-// var clients = make(map[*websocket.Conn]bool) // connected clients
-// var broadcast = make(chan Message)           // channel to broadcast messages
-// var upgrader = websocket.Upgrader{}          // upgrader for websockets
-
 func main() {
 	hub := makeHub()
 	r := mux.NewRouter()
@@ -34,60 +30,3 @@ func main() {
 
 	log.Fatal(srv.ListenAndServe())
 }
-
-// func main() {
-// 	http.HandleFunc("/ws", handleWebSockets) // websocket initiation route
-// 	go handleMessages()                      // goroutine to handle sending out messages
-// 	err := http.ListenAndServe(":8000", nil)
-// 	if err != nil {
-// 		log.Fatal("ListenAndServe: ", err)
-// 	}
-// }
-
-/**
-func handleWebSockets(w http.ResponseWriter, r *http.Request) {
-	upgrader.CheckOrigin = func(*http.Request) bool { return true } // allow requests from wherever
-	ws, err := upgrader.Upgrade(w, r, nil)                          // upgrade http request to web socket
-	if err != nil {
-		log.Fatal("Upgrade: ", err)
-	}
-
-	defer ws.Close() // make sure to close websocket once functions finishes
-
-	clients[ws] = true // add new websocket to global map
-
-	// loop that accepts messages and broadcasts to channel
-	for {
-		var msg Message
-
-		err := ws.ReadJSON(&msg) // read in message and parse as json
-		if err != nil {
-			log.Printf("error ReadJSON: %v", err)
-			delete(clients, ws) // remove websocket from global map
-			break
-		}
-		fmt.Println("got a message")
-		fmt.Printf("%v", msg)
-
-		broadcast <- msg // send received message to broadcast channel
-	}
-}
-
-func handleMessages() {
-	for {
-		msg := <-broadcast // get message from channel
-		fmt.Println("message from broadcast channel")
-		fmt.Printf("%v", msg)
-
-		// send message to connected web sockets
-		for client := range clients {
-			err := client.WriteJSON(msg)
-			if err != nil {
-				log.Printf("error WriteJSON: %v", err)
-				client.Close()          // close web socket connection
-				delete(clients, client) // remove from global map
-			}
-		}
-	}
-}
-**/
